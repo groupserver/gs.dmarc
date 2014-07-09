a0bca548954f8542d230cea59ac4b434c0d0dfed
Michael JasonSmith <mpj17@onlinegroups.net>
2014-07-09 11:00:13 -0700

    OCD PEP-8 reformat of the setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+############################################################################
 #
 # Copyright © 2014 OnlineGroups.net and Contributors.
 # All Rights Reserved.
@@ -11,13 +11,20 @@
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
 #
-##############################################################################
+############################################################################
+import codecs
 import os
 import sys
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
+
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"),
+                 encoding='utf-8') as f:
+    long_description += '\n' + f.read()
 
 requires = ['publicsuffix', ]
 if (sys.version_info < (3, 4)):
@@ -28,39 +35,38 @@ else:
     requires += ['dnspython >= 1.11.1']
 
 setup(name='gs.dmarc',
-    version=version,
-    description="Look up and report on the DMARC status of a domain.",
-    long_description=open("README.txt").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.txt")).read(),
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        "Intended Audience :: Developers",
-        'License :: OSI Approved :: Zope Public License',
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+      version=version,
+      description="Look up and report on the DMARC status of a domain.",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          "Intended Audience :: Developers",
+          'License :: OSI Approved :: Zope Public License',
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy",
+          "Topic :: Software Development :: Libraries :: Python Modules",
       ],
-    keywords='DKIM, SPF, DMARC, email',
-    author='Michael JasonSmith',
-    author_email='mpj17@onlinegroups.net',
-    url='https://source.iopen.net/groupserver/gs.dmarc/',
-    license='ZPL 2.1',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs'],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=requires,
-    extras_require={'docs': ['Sphinx', 'markupsafe'], },
-    test_suite="gs.dmarc.tests.test_all",
-    tests_require=['mock', ],
-    entry_points="""
-    # -*- Entry points: -*-
-    """,)
+      keywords='DKIM, SPF, DMARC, email',
+      author='Michael JasonSmith',
+      author_email='mpj17@onlinegroups.net',
+      url='https://source.iopen.net/groupserver/gs.dmarc/',
+      license='ZPL 2.1',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gs'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      extras_require={'docs': ['Sphinx', ], },
+      test_suite="gs.dmarc.tests.test_all",
+      tests_require=['mock', ],
+      entry_points="""
+          # -*- Entry points: -*-
+      """,)
