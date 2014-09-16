aae2c703f74559e6c87cabe8bfd2016ca5bb76c4
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-16 14:11:52 -0700

    PEP-8 OCD

--- gs/dmarc/tests/test_all.py
+++ gs/dmarc/tests/test_all.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+############################################################################
 #
 # Copyright © 2014 OnlineGroups.net and Contributors.
 # All Rights Reserved.
@@ -11,7 +11,7 @@
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
 #
-##############################################################################
+############################################################################
 from __future__ import absolute_import, unicode_literals
 from unittest import TestSuite, main as unittest_main
 from gs.dmarc.tests.lookup import TestLookup
